f64ec168b3e2e5ae3066684dd57e1de2cdc8eb0f
jijundu <jijundu@tencent.com>
2025-01-03 09:46:42 +0800

    [Enhancement] Make loading run faster

--- scripts/insert-into/catalog_sales.sql
+++ scripts/insert-into/catalog_sales.sql
@@ -1,2 +1,2 @@
-INSERT OVERWRITE TABLE catalog_sales SELECT /*+ COALESCE(20000)*/ * FROM catalog_sales_ext cs;
+INSERT OVERWRITE TABLE catalog_sales SELECT /*+ COALESCE(72)*/ * FROM catalog_sales_ext cs;
 analyze table catalog_sales COMPUTE STATISTICS  for all columns;
--- scripts/insert-into/store_sales.sql
+++ scripts/insert-into/store_sales.sql
@@ -1,2 +1,2 @@
-INSERT OVERWRITE TABLE store_sales SELECT /*+ COALESCE(2000)*/ * FROM store_sales_ext ss;
+INSERT OVERWRITE TABLE store_sales SELECT /*+ COALESCE(72)*/ * FROM store_sales_ext ss;
 analyze table store_sales COMPUTE STATISTICS  for all columns;
