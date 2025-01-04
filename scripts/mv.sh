8a2d9ae680dcf7b9aa5b95c699efd72a612cda04
jijundu <jijundu@tencent.com>
2025-01-04 20:43:36 +0800

    [Enhancement] Add mv

---
+++ scripts/mv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 检查输入参数的个数
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <target_path>"
+    exit 1
+fi
+
+# 读取输入的目标路径和本地路径
+target_path=$1
+mv ${target_path}/store_sales_ext/store_returns_*.dat ${target_path}/store_returns_ext/
+mv ${target_path}/web_sales_ext/web_returns_*.dat ${target_path}/web_returns_ext/
+mv ${target_path}/catalog_sales_ext/catalog_returns_*.dat ${target_path}/catalog_returns_ext/
