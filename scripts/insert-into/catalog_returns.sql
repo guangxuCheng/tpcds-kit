5189cf6d37c7936434146c3c3439217deea3ea6f
jijundu <jijundu@tencent.com>
2025-01-10 15:16:40 +0800

    [Enhancement] Optimize to use distributed bucket

--- scripts/insert-into/catalog_returns.sql
+++ scripts/insert-into/catalog_returns.sql
@@ -1,2 +1,2 @@
-INSERT OVERWRITE TABLE catalog_returns * FROM catalog_returns_ext DISTRIBUTE BY cr_returned_date_sk, cr_item_sk;
+INSERT OVERWRITE TABLE catalog_returns * FROM catalog_returns_ext DISTRIBUTE BY cr_returned_date_sk, bucket(3, cr_item_sk);
 analyze table catalog_returns COMPUTE STATISTICS  for all columns;
--- scripts/insert-into/catalog_sales.sql
+++ scripts/insert-into/catalog_sales.sql
@@ -1,2 +1,2 @@
-INSERT OVERWRITE TABLE catalog_sales SELECT * FROM catalog_sales_ext cs DISTRIBUTE BY cs_sold_date_sk, cs_item_sk;
+INSERT OVERWRITE TABLE catalog_sales SELECT * FROM catalog_sales_ext cs DISTRIBUTE BY cs_sold_date_sk, bucket(3, cs_item_sk);
 analyze table catalog_sales COMPUTE STATISTICS  for all columns;
