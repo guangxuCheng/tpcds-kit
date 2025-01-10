86d8a6058d6c3bfc40b74ce016f4f361927096a7
jijundu <jijundu@tencent.com>
2025-01-10 19:23:45 +0800

    [BugFix] Fix syntax bug

--- scripts/insert-into/catalog_returns.sql
+++ scripts/insert-into/catalog_returns.sql
@@ -1,2 +1,2 @@
-INSERT OVERWRITE TABLE catalog_returns * FROM catalog_returns_ext DISTRIBUTE BY cr_returned_date_sk;
+INSERT OVERWRITE TABLE catalog_returns SELECT * FROM catalog_returns_ext DISTRIBUTE BY cr_returned_date_sk;
 analyze table catalog_returns COMPUTE STATISTICS  for all columns;
