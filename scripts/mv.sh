465a115249c1a7587aa31ca5091517a091da321a
shaopenquan <shaopenquan@tencent.com>
2025-05-16 19:09:14 +0800

    update mv.sh

--- scripts/mv.sh
+++ scripts/mv.sh
@@ -8,6 +8,17 @@ fi
 
 # 读取输入的目标路径和本地路径
 target_path=$1
-mv ${target_path}/store_sales_ext/store_returns_*.dat ${target_path}/store_returns_ext/
-mv ${target_path}/web_sales_ext/web_returns_*.dat ${target_path}/web_returns_ext/
-mv ${target_path}/catalog_sales_ext/catalog_returns_*.dat ${target_path}/catalog_returns_ext/
+#mv ${target_path}/store_sales_ext/store_returns_*.dat ${target_path}/store_returns_ext/
+#mv ${target_path}/web_sales_ext/web_returns_*.dat ${target_path}/web_returns_ext/
+#mv ${target_path}/catalog_sales_ext/catalog_returns_*.dat ${target_path}/catalog_returns_ext/
+for item in ${target_path}/store_sales_ext/store_returns_*.dat; do
+    mv $item ${target_path}/store_returns_ext/
+done
+
+for item in ${target_path}/web_sales_ext/store_returns_*.dat; do
+    mv $item ${target_path}/web_returns_ext/
+done
+
+for item in ${target_path}/catalog_sales_ext/store_returns_*.dat; do
+    mv $item ${target_path}/catalog_returns_ext/
+done
